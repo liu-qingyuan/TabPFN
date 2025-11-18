868ce0ec373651d7ac6079cc68c62f37d1699093
liu-qingyuan <2981185462@qq.com>
2025-11-18 13:51:46 +0800

    8:8:8:8的复原结果重现

--- job_script.sh
+++ job_script.sh
@@ -32,7 +32,10 @@ cd uda_medical_imbalance_project
 # python tests/debug_feature_selection.py
 # python tests/test_uda_visualization.py
 # python examples/real_data_visualization.py
-python scripts/run_complete_analysis.py
+# python scripts/run_complete_analysis.py
+
+python scripts/run_complete_analysis.py --feature_set best8
+
 # python scripts/generate_baseline_characteristics_table.py
 
 # python debug_calibration.py
